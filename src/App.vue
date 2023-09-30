3aa8c51964d068281bdfd8e83d2e152569fd9e8d
Sacramentix <benji.u1225u@gmail.com>
2023-09-30 22:24:30 +0200

    initial commit made with 'npm create vite'

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <h1>A tale to the limit of space</h1>
+</template>
+
+<style>
+html, body {
+  padding: 0;
+  margin: 0;
+}
+body {
+  background: #000;
+  color: #eee;
+  width: 100%;
+  min-height: 100vh;
+}
+#app {
+
+}
+</style>
